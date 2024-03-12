0c273b488f9e58a6f24822549a983b939600ce54
Jackson Pais <jacksonpais@gmail.com>
2024-03-12 17:00:51 +0530

    added images

--- src/Container/Rock-Paper-Scissor/Circle.tsx
+++ src/Container/Rock-Paper-Scissor/Circle.tsx
@@ -1,11 +1,17 @@
 const Circle = (props: any) => {
+  let activeClass = props.selected !== false ? "bg-yellow-500" : "";
   return (
     <div
-      className="w-48 h-48 m-2 border-4 border-yellow-500 text-white rounded-full"
+      className={`w-48 h-48 m-2 border-4 border-yellow-500 ${activeClass} text-white rounded-full`}
       onClick={props.handleClick}
     >
       <div className="h-48 flex items-center justify-center">
-        <div>{props.value}</div>
+        <div>
+          <img
+            className="object-cover w-44 h-44 rounded-full mb-2"
+            src={`/images/rock-paper-scissor/${props.value}.png`}
+          />
+        </div>
       </div>
     </div>
   );
